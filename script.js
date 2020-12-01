9f320a6bff2c1f91566f2882251e2d4d2cf68906
Scrirock <73163680+Scrirock@users.noreply.github.com>
2020-12-01 15:01:13 +0100

    ternaire

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
 for (let i = 1; i <= 100; i++){
-    if ((i % 3 === 0) && (i % 5 !== 0)) {
-        document.getElementById("text").innerHTML += "fizz<br>";
-    }
-    else if ((i % 5 === 0) && (i % 3 !== 0)) {
-        document.getElementById("text").innerHTML += "buzz<br>";
-    }
-
-    else if ((i % 3 === 0) && (i % 5 === 0)) {
-        document.getElementById("text").innerHTML += "fizzbuzz<br>";
-    }
-
-    else {
-        document.getElementById("text").innerHTML += i+"<br>";
-    }
+    i % 3 === 0 && i % 5 === 0 ? document.getElementById("text").innerHTML += "fizzbuzz<br>" :
+        i % 3 === 0 ? document.getElementById("text").innerHTML += "fizz<br>" :
+            i % 5 === 0 ? document.getElementById("text").innerHTML += "buzz<br>" :
+                document.getElementById("text").innerHTML += i+"<br>"
 }
